14ec3a84d2e42745beef19078c19ae2ce3b94ef8
Emilse Arias <emilsearias@gmail.com>
2024-02-19 08:41:53 -0300

    agregue el maquetado del proyecto rollingcoffee

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Footer from "./components/common/Footer";
+import Menu from "./components/common/Menu";
+import Error404 from "./components/pages/Error404";
+import Administrador from "./components/pages/Administrador";
+import Inicio from "./components/pages/Inicio";
+import FormularioProducto from "./components/pages/producto/FormularioProducto";
+import DetalleProducto from "./components/pages/DetalleProducto";
+
+function App() {
+  return (
+  <Menu></Menu>
+  );
+}
+
+export default App;
---
+++ src/components/common/Menu.jsx
@@ -0,0 +1,26 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../../assets/coffee_Logo.png'
+import { Link, NavLink } from "react-router-dom";
+
+const Menu = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="Logo de RollingCoffee" className="img-fluid" width={150}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <NavLink end className="nav-link" to="/">Inicio</NavLink>
+              <NavLink end className="nav-link" to="/administrador">Administrador</NavLink>
+              <NavLink end className="nav-link" to="/registro">Registro</NavLink>
+              <NavLink end className="nav-link" to="/login">Login</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+};
+
+export default Menu;
